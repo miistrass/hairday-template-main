9236c23adb1ef2bac4af4e9494b1680fe84ff61b
Milena Strassburger <milenastrass@gmail.com>
2025-02-03 08:52:52 -0300

    blocking already scheduled times

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -7,16 +7,21 @@ const isMorning = "9:00"
 const isAfternoon = "13:00"
 const isNight = "18:00"
 
-export function hoursLoad({ date }) {
+export function hoursLoad({ date, dailySchedules }) {
   hours.innerHTML = ""
+
+  const unavailableHours = dailySchedules.map((schedule) => dayjs(schedule.when).format("HH:mm"))
+
   const opening = openingHours.map((hour) => {
     const [scheduleHour] = hour.split(":");
 
-    const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs());
+    const isHourPast = dayjs(date).add(scheduleHour, "hour").isBefore(dayjs());
+
+    const avaliable = !unavailableHours.includes(hour) && !isHourPast
 
     return {
         hour,
-        avaliable: isHourPast,
+        avaliable
     }
   });
 
