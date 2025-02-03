f7bac52ac0c7d493cb3d3822058cd448903a6683
Milena Strassburger <milenastrass@gmail.com>
2025-02-03 08:58:40 -0300

    updating appointment list

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs"
 
 import { scheduleNew } from "../../services/schedule-new.js"
+import { schedulesDay } from "../schedules/load.js"
 
 const form = document.querySelector("form")
 const clientName = document.getElementById("client")
@@ -37,6 +38,9 @@ form.onsubmit = async (event) => {
             when,
         })
 
+        await schedulesDay()
+
+        clientName.value = ""
     } catch (error) {
         alert("Não foi possível realizar o agendamento")
         console.log(error)
